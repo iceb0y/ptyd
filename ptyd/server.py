cf4636f702f5978f669404407996ec60a819c628
iceboy <me@iceboy.org>
2017-04-24 21:40:16 -0700

    it works

--- ptyd/server.py
+++ ptyd/server.py
@@ -1,31 +1,58 @@
-from asyncio import get_event_loop
-from functools import partial
-from os import close, kill, execv, forkpty, path, read
+from asyncio import get_event_loop, wait, FIRST_COMPLETED
+from os import close, execvp, forkpty, path, read, write, waitpid, WNOHANG
 from sanic import Sanic
-from sanic.response import text
-from signal import SIGTERM
+from signal import SIGCHLD
+from sys import argv, exit
+
+if not argv[1:]:
+    print('Usage: ptyd <command> [<arguments...>]')
+    exit(1)
 
 static_dir = path.join(path.dirname(__file__), 'static')
 app = Sanic()
 app.static('', static_dir)
 app.static('', path.join(static_dir, 'app.html'))
 
-@app.route('hello')
-async def hello(request):
-    loop = get_event_loop()
+def prepare(_, loop):
+    loop.add_signal_handler(SIGCHLD, sigchld)
+
+def sigchld():
+    while True:
+        try:
+            waitpid(-1, WNOHANG)
+        except ChildProcessError:
+            return
+
+@app.websocket('pty')
+async def pty(_, ws):
     pid, fd = forkpty()
     if not pid:
-        execv('/bin/bash', ['bunny'])
-    loop.add_reader(fd, partial(on_read, loop, fd))
-    loop.call_later(1, partial(kill, pid, SIGTERM))
-    return text('Hello world!')
+        execvp(argv[1], argv[1:])
 
-def on_read(loop, fd):
+    loop = get_event_loop()
+    fd_to_ws_task = loop.create_task(fd_to_ws(fd, ws))
+    ws_to_fd_task = loop.create_task(ws_to_fd(ws, fd))
     try:
-        print(read(fd, 4096))
-    except Exception as e:
-        print(e)
-        loop.remove_reader(fd)
+        await wait([fd_to_ws_task, ws_to_fd_task], return_when=FIRST_COMPLETED)
+    finally:
+        if not fd_to_ws_task.done():
+            fd_to_ws_task.cancel()
+        if not ws_to_fd_task.done():
+            ws_to_fd_task.cancel()
         close(fd)
 
-app.run()
+async def fd_to_ws(fd, ws):
+    loop = get_event_loop()
+    while True:
+        data = await loop.run_in_executor(None, read, fd, 256)
+        if not data:
+            return
+        await ws.send(data.decode(errors='replace'))
+
+async def ws_to_fd(ws, fd):
+    loop = get_event_loop()
+    while True:
+        data = (await ws.recv()).encode()
+        await loop.run_in_executor(None, write, fd, data)
+
+app.run(before_start=prepare)
